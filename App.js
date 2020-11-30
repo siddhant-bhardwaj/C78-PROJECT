7edc76c80b6d88d3c82051d72b12f617b15df4bc
siddhant-bhardwaj <61611845+siddhant-bhardwaj@users.noreply.github.com>
2020-11-30 19:38:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import WelcomeScreen from './screens/welcomeScreen';
+export default function App() {
+  return (
+    <WelcomeScreen />
+  );
+}
+
+
